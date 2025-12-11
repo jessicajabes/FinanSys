5e3feaff9981aa11bdbe9f4eccc32490662cb9cf
jessicajabes <jessicajabes@hotmail.com>
2025-12-11 12:30:37 -0300

    feat: implement cardsController

--- server/src/controllers/cardsController.js
+++ server/src/controllers/cardsController.js
@@ -47,3 +47,43 @@ const getById = async(req, res) => {
         })
     }
 }
+
+const update = async(req,res)=>{
+    try{
+        const rawId = req.params.id
+        const id = Number(rawId)
+        if(!id) return res.status(400).json({
+            error: "Id inválido"
+        })
+        const patch = req.body || {}
+        const card = cardsModel.update(id, patch)
+        if(!card) res.status(404).json({
+            error: 'Banco não encontrado'
+        })
+        return res.json()
+    }catch{
+        console.error('Erro ao atualizar o cartão')
+        res.status(500).json({
+            error: "Erro interno do servidor"
+        })
+    }
+}
+
+const remove = async (req, res) => {
+    try{
+        const id = Number(req.params.id)
+        if (!id) return res.status(400).json({ error: 'ID inválido' })
+          await cardsModel.remove(id)
+        return res.status(204).send()  
+    }catch(error) {
+        console.error('Erro ao remover cartão: ', error)
+        return res.status(500).json({ error: 'Erro interno do servidor'})
+    }
+}
+
+export default{
+    create,
+    update,
+    getById,
+    remove,
+}
