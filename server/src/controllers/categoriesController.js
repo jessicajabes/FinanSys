278fe48d2517bda3f37fa8fa618d1859b9471768
jessicajabes <jessicajabes@hotmail.com>
2025-12-11 12:52:04 -0300

    feat: implement categoriesController

---
+++ server/src/controllers/categoriesController.js
@@ -0,0 +1,49 @@
+import db from '../config/database.js'
+import banksModel from '../models/banksModel.js'
+import categoriesModel from '../models/categoriesModel.js'
+
+const create = async (req, res) => {
+    let client = null
+    try {
+        client = await db.pool.connect()
+        try {
+            await client.query('BEGIN')
+            const newCategorie = await categoriesModel.create(client, req.body)
+            await client.query('COMMIT')
+            return res.status(201).json({
+                message: 'Categoria criada com sucesso',
+                categorie: newCategorie,
+            })
+        } catch (error) {
+            if (client) await client.query('ROLLBACK')
+            console.error('Erro ao criar registro', error)
+            return res.status(500).json({ error: 'Erro interno do servidor' })
+        }
+    } catch (error) {
+        console.error('Erro no registro', error)
+        return res.status(500).json({ error: 'Erro interno do servidor' })
+    } finally {
+        if (client) client.release()
+    }
+}
+
+const getById = async (req,res) => {
+    try{
+        const rawId = req.params.id ?? req.query.id
+        const id = Number(rawId)
+        if (!id) return res.status(400).json({ error: 'ID inválido' })
+        const categorie = await categoriesModelModel.findById(id)
+        if (!categorie) return res.status(404).json({ error: 'Categoria não encontrada' })
+        return res.json({ categorie })
+    }catch (error){
+        console.error('Erro ao buscar categoria', error)
+        return res.status(500).json({ error: 'Erro interno do servidor'})
+    }
+}
+
+export default{
+    create,
+    update,
+    getById,
+    remove
+}
